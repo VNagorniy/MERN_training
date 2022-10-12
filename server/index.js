7f76f65aad385b8a125606c92e9cc11c9a8f5f7d
Doom <vnagorniyrnd@gmail.com>
2022-10-12 23:36:18 +0300

    Подключение к mongodb, создание модели user

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+// Создаём сервер
+const app = express();
+
+const PORT = config.get('serverPort');
+
+//Ф-я подключения к БД и запуск сервера
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbUrl'));
+    app.listen(PORT, () => {
+      console.log(`Сервер запущен на `, PORT);
+    });
+  } catch (e) {}
+};
+
+start();
