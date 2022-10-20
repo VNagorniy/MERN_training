3e4ab5c1d9dd597071be1bf8ca1aea49dee15773
VNagorniy <vnagorniyrnd@gmail.com>
2022-10-20 21:24:06 +0300

    JWT токен, авторизация

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const router = new Router();
 
@@ -25,7 +27,7 @@ router.post(
         return res.status(400).json({ message: `Пользователь с таким email ${email} уже создан` });
       }
       //Хэширование пароля
-      const hashPassword = await bcrypt.hash(password, 15);
+      const hashPassword = await bcrypt.hash(password, 8);
       const user = new User({ email, password: hashPassword });
       await user.save();
       return res.json({ message: 'Пользователь создан' });
@@ -35,5 +37,35 @@ router.post(
     }
   }
 );
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'Пользователь не найден' });
+    }
+    //Сравнение пароль в БД с написанным
+    const isPassValid = bcrypt.compareSync(password, user.password);
+    if (!isPassValid) {
+      return res.status(400).json({ message: 'Некорректный пароль' });
+    }
+    //Создадим токен (sign принимает: объект с данными, секретный ключ,объект времени хранения токена)
+    const token = jwt.sign({ id: user.id }, config.get('secretKey'), { expiresIn: '1h' });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: 'Ошибка сервера' });
+  }
+});
 
 module.exports = router;
