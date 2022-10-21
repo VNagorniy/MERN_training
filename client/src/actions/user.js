bab3c0af6a057b101bb0800ab8b92169d66caa00
VNagorniy <vnagorniyrnd@gmail.com>
2022-10-22 00:40:41 +0300

    Форма входа и выхода,взаимодействие с JWT

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { setUser } from '../reducers/userReducers';
 
 export const registration = async (email, password) => {
   try {
@@ -10,4 +11,35 @@ export const registration = async (email, password) => {
   } catch (e) {
     alert(e.response.data.message);
   }
+};
+
+//Функция запроса логина на сервер
+export const login = (email, password) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(`http://localhost:5000/api/auth/login`, {
+        email,
+        password,
+      });
+      dispatch(setUser(response.data.user));
+      //Хранение токена
+      localStorage.setItem('token', response.data.token);
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
+};
+
+//Функция запроса на авторизацию
+export const auth = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/auth/auth`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } });
+      dispatch(setUser(response.data.user));
+      localStorage.setItem('token', response.data.token);
+    } catch (e) {
+      alert(e.response.data.message);
+      localStorage.removeItem('token');
+    }
+  };
 };
---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+//Открываем приложение - получаем данные о пользователе (по токену получает пользователя и возврщает его обратно)
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Auth error' });
+    }
+    const decoded = jwt.verify(token, config.get('secretKey'));
+    req.user = decoded;
+    next();
+  } catch (e) {
+    return res.status(401).json({ message: 'Auth error' });
+  }
+};
